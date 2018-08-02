914d371c1f7d2521b116295a0118727242686f0d
KingKang <k15950590137@126.com>
2018-08-02 11:38:40 +0800

    自定义页面渲染

---
+++ SharePoint-Hosted-Add-in/SharePoint-Hosted-Add-in/Scripts/CustomRenders/LevelRender.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    /**
+     * 设置修改覆盖样式选项
+     * */
+    var templateOverridesOptions = {};
+    templateOverridesOptions.Templates = {};
+    //templateOverridesOptions.Templates.Header = renderHeader;
+    templateOverridesOptions.Templates.Fields = {
+        "Level": { "View": renderItems}
+    }
+
+
+    SPClientTemplates.TemplateManager.RegisterTemplateOverrides(templateOverridesOptions);
+})();
+/**
+ * 渲染列表
+ * */
+function renderItems(ctx) {
+    console.log(ctx);
+    var Level = ctx.CurrentItem[ctx.CurrentFieldSchema.Name];
+    if (Level > 2) {
+        return "<p style='color:blue'>" + Level + "</p>";
+    } else {
+        return Level;
+    }
+}
+function renderHeader(renderContext) {
+    console.log(renderContext);
+    return "heder";
+}
